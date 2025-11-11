17aeabf2560068f7af99f75c91825592caf53113
Sam Potter <spotter1642@gmail.com>
2025-11-11 05:55:33 -0500

    Add a run func to main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func run(
+	ctx context.Context,
+	args []string,
+	getenv func(string) string,
+	stdin io.Reader,
+	stdout, stderr io.Writer,
+) error {
+	fmt.Println("Hello, World")
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	ctx := context.Background()
+	if err := run(
+		ctx,
+		nil,
+		nil,
+		nil,
+		os.Stdout,
+		os.Stderr,
+	); err != nil {
+		log.Fatalf("%s", err)
+	}
 }
