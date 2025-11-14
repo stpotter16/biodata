6451836b5e2ca29c37898c964d2dd2edccf2dc9a
Sam Potter <spotter1642@gmail.com>
2025-11-13 21:03:58 -0500

    Add start of index page

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Track basic biodata
 
 # TODO
-- Landing page
+- Index page
 - Build a go linter
 - Install shellcheck
 - Build a shell linter
+- Install prettier
+- Build a html linter
--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -1,14 +1,21 @@
 package handlers
 
 import (
+	"embed"
+	"html/template"
 	"net/http"
 )
 
+//go:embed templates
+var templateFS embed.FS
+
 func indexGet() http.HandlerFunc {
+	t := template.Must(
+		template.New("base.html").
+			ParseFS(
+				templateFS,
+				"templates/layouts/base.html", "templates/pages/index.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusOK)
-		response := []byte("Hello from biodata server")
-		w.Write(response)
+		t.Execute(w, nil)
 	}
 }
