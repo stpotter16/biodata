b45648fe98b134664ebee98f66ac9403d508dc44
Sam Potter <spotter1642@gmail.com>
2025-11-17 05:23:33 -0500

    Add an edit entry page

--- internal/handlers/entry.go
+++ internal/handlers/entry.go
@@ -15,3 +15,14 @@ func newEntryGet() http.HandlerFunc {
 		t.Execute(w, nil)
 	}
 }
+
+func editEntryGet() http.HandlerFunc {
+	t := template.Must(
+		template.New("base.html").
+			ParseFS(
+				templateFS,
+				"templates/layouts/base.html", "templates/pages/edit_entry.html"))
+	return func(w http.ResponseWriter, r *http.Request) {
+		t.Execute(w, nil)
+	}
+}
--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -5,5 +5,6 @@ import "net/http"
 func addRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /{$}", indexGet())
 	mux.HandleFunc("GET /entry/new", newEntryGet())
+	mux.HandleFunc("GET /entry/{date}/edit", editEntryGet())
 	mux.Handle("GET /static/", http.StripPrefix("/static/", serveStaticFiles()))
 }
