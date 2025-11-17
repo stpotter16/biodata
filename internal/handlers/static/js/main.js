718ad4040d16b91abbdee43f59e10b0a24e6414d
Sam Potter <spotter1642@gmail.com>
2025-11-17 05:19:17 -0500

    Add actions column

--- internal/handlers/static/js/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('tbody tr').forEach(row => {
-    row.addEventListener('click', () => {
-        window.location.href = `/entry/${row.cells[0].textContent}/edit`;
-    });
-});
