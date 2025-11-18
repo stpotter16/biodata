af2c4aa262c672f57e2633fc55a3c2141d7f68f6
Sam Potter <spotter1642@gmail.com>
2025-11-18 06:05:44 -0500

    Start new entry handler

--- internal/handlers/entry.go
+++ internal/handlers/entry.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func entriesGet() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +19,22 @@ func entryGet() http.HandlerFunc {
 
 func entryPost() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		body := struct {
+			Date   string `json:"date"`
+			Weight string `json:"weight"`
+			Waist  string `json:"waist"`
+			BP     string `json:"bp"`
+		}{}
+
+		decoder := json.NewDecoder(r.Body)
+
+		if err := decoder.Decode(&body); err != nil {
+			log.Printf("Invalid auth request: %v", err)
+			http.Error(w, fmt.Sprintf("Invalid new entry request: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Received %v", body)
 	}
 }
 
