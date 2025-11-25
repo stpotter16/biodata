d1223bde1bd45d32032fe4beddc9cb18a80cee6b
Sam Potter <spotter1642@gmail.com>
2025-11-25 10:51:58 -0500

    Add initial migration logic

--- internal/store/sqlite/migrations.go
+++ internal/store/sqlite/migrations.go
@@ -1,5 +1,102 @@
 package sqlite
 
+import (
+	"cmp"
+	"context"
+	"embed"
+	"fmt"
+	"log"
+	"path"
+	"slices"
+	"strconv"
+)
+
+type migration struct {
+	version   int
+	statement string
+}
+
+//go:embed migrations/*.sql
+var migrationFs embed.FS
+
 func (s Store) runMigrations() error {
+	var currentSchemaVersion int
+	// TODO: What to do with the context here...
+	if err := s.db.QueryRow(context.TODO(), &currentSchemaVersion, `PRAGMA user_version`); err != nil {
+		log.Printf("Could not read the current schema version: %v", err)
+		return err
+	}
+
+	migrations, err := loadMigrations()
+	if err != nil {
+		log.Printf("Could not load database migrations: %v", err)
+		return err
+	}
+
+	log.Printf("Current database schema version is %d out of %d migrations", currentSchemaVersion, len(migrations))
+
+	for _, migration := range migrations {
+		if currentSchemaVersion >= migration.version {
+			continue
+		}
+
+		transactions := []string{
+			migration.statement,
+			fmt.Sprintf(`PRAGMA user_version=%d`, migration.version),
+		}
+		// TODO: What to do with the context here...
+		if err := s.db.ExecuteTransaction(context.TODO(), transactions...); err != nil {
+			log.Printf("Could not execute migration: %v", err)
+			return err
+		}
+
+		log.Printf("Current database version is %d out of %d migrations", migration.version, len(migrations))
+	}
+
 	return nil
+}
+
+func loadMigrations() ([]migration, error) {
+	migrations := []migration{}
+
+	migrationsDir := "migrations"
+
+	migrationEntries, err := migrationFs.ReadDir(migrationsDir)
+	if err != nil {
+		return []migration{}, err
+	}
+
+	for _, entry := range migrationEntries {
+		if entry.IsDir() {
+			continue
+		}
+
+		version, err := parseVersionNumber(entry.Name())
+		if err != nil {
+			return []migration{}, err
+		}
+
+		statement, err := migrationFs.ReadFile(path.Join(migrationsDir, entry.Name()))
+		if err != nil {
+			return []migration{}, err
+		}
+
+		migrations = append(migrations, migration{version, string(statement)})
+	}
+
+	slices.SortFunc(migrations,
+		func(a, b migration) int {
+			return cmp.Compare(a.version, b.version)
+		})
+
+	return migrations, nil
+}
+
+func parseVersionNumber(name string) (int, error) {
+	version, err := strconv.ParseInt(name[:3], 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(version), nil
 }
---
+++ internal/store/sqlite/migrations/001_entries.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS entry (
+    id INTEGER PRIMARY KEY,
+    date TEXT NOT NULL,
+    weight REAL,
+    waist REAL,
+    bp TEXT,
+    created TEXT NOT NULL,
+    last_modified TEXT NOT NULL
+) STRICT;
+
+CREATE INDEX IF NOT EXISTS idx_entry_date ON entry(date);
