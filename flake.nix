3584bad551e591e1b9024d2e3a90846349de23b7
Sam Potter <spotter1642@gmail.com>
2025-11-26 08:52:38 -0500

    Add sqlite to the flake

--- flake.nix
+++ flake.nix
@@ -6,16 +6,21 @@
 
     # 1.25.2 release
     go-nixpkgs.url = "github:NixOS/nixpkgs/01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09";
+
+    # 3.50.4 release
+    sqlite-nixpkgs.url="github:NixOS/nixpkgs/6f374686605df381de8541c072038472a5ea2e2d";
   };
 
   outputs = {
     self,
     flake-utils,
     go-nixpkgs,
+    sqlite-nixpkgs,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       gopkg = go-nixpkgs.legacyPackages.${system};
       go = gopkg.go_1_25;
+      sqlite = sqlite-nixpkgs.legacyPackages.${system}.sqlite;
     in {
       devShells.default = gopkg.mkShell {
         packages = [
@@ -27,6 +32,7 @@
           gopkg.godef
           gopkg.golint
           go
+          sqlite
         ];
 
         shellHook = ''
@@ -35,6 +41,7 @@
           export GOROOT="${go}/share/go"
 
           go version
+          echo "sqlite" "$(sqlite3 --version | cut -d ' ' -f 1-2)"
         '';
       };
 
