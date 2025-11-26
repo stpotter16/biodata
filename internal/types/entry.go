970056fa4206cfef811945054d290e56e9f8184b
Sam Potter <spotter1642@gmail.com>
2025-11-26 10:35:55 -0500

    Add types package with EntryDTO as first type

---
+++ internal/types/entry.go
@@ -0,0 +1,11 @@
+package types
+
+type EntryDTO struct {
+	id            int
+	date          string
+	weight        float64
+	waist         float64
+	bp            string
+	created       string
+	last_modified string
+}
