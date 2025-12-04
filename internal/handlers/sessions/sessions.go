dc7f1ff79ff617d8af61f6ed27c7654768cc5964
Sam Potter <spotter1642@gmail.com>
2025-12-04 10:37:15 -0500

    Add a session manager

---
+++ internal/handlers/sessions/sessions.go
@@ -0,0 +1,15 @@
+package sessions
+
+import "github.com/stpotter16/biodata/internal/store/db"
+
+type SessionManger struct {
+	db *db.DB
+}
+
+func New(db db.DB) SessionManger {
+	s := SessionManger{
+		db: &db,
+	}
+
+	return s
+}
