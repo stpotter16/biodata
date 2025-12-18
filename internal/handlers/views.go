60206e97e81283a8b69559ee9675a4afff7ef4dc
Sam Potter <spotter1642@gmail.com>
2025-12-18 12:10:40 -0500

    Pass placeholder csrftoken to html pages

--- internal/handlers/views.go
+++ internal/handlers/views.go
@@ -11,6 +11,10 @@ import (
 	"github.com/stpotter16/biodata/internal/types"
 )
 
+type viewProps struct {
+	CsrfToken string
+}
+
 //go:embed templates
 var templateFS embed.FS
 
@@ -28,7 +32,12 @@ func loginGet() http.HandlerFunc {
 				templateFS,
 				"templates/layouts/base.html", "templates/pages/login.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := t.Execute(w, nil); err != nil {
+		props := viewProps{CsrfToken: ""}
+		if err := t.Execute(w, struct {
+			viewProps
+		}{
+			viewProps: props,
+		}); err != nil {
 			log.Printf("Could not create login page: %v", err)
 			http.Error(w, "Server issue - try again later", http.StatusInternalServerError)
 		}
@@ -43,6 +52,7 @@ func indexGet(store store.Store) http.HandlerFunc {
 				templateFS,
 				"templates/layouts/base.html", "templates/pages/index.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
+		props := viewProps{CsrfToken: ""}
 		entries, err := store.GetEntries(r.Context())
 		if err != nil {
 			log.Printf("Could not read existing entries: %v", err)
@@ -50,9 +60,11 @@ func indexGet(store store.Store) http.HandlerFunc {
 			return
 		}
 		if err = t.Execute(w, struct {
+			viewProps
 			Entries []types.Entry
 		}{
-			Entries: entries,
+			viewProps: props,
+			Entries:   entries,
 		}); err != nil {
 			log.Printf("Could not create entry page: %v", err)
 			http.Error(w, "Server issue - try again later", http.StatusInternalServerError)
@@ -68,7 +80,12 @@ func newEntryGet() http.HandlerFunc {
 				templateFS,
 				"templates/layouts/base.html", "templates/pages/new_entry.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := t.Execute(w, nil); err != nil {
+		props := viewProps{CsrfToken: ""}
+		if err := t.Execute(w, struct {
+			viewProps
+		}{
+			viewProps: props,
+		}); err != nil {
 			log.Printf("Could not create new entry page: %v", err)
 			http.Error(w, "Server issue - try again later", http.StatusInternalServerError)
 		}
@@ -83,6 +100,7 @@ func editEntryGet(store store.Store) http.HandlerFunc {
 				templateFS,
 				"templates/layouts/base.html", "templates/pages/edit_entry.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
+		props := viewProps{CsrfToken: ""}
 		entryDateStr := r.PathValue("date")
 		entryDate, err := time.Parse("2006-01-02", entryDateStr)
 		if err != nil {
@@ -97,9 +115,11 @@ func editEntryGet(store store.Store) http.HandlerFunc {
 			return
 		}
 		if err = t.Execute(w, struct {
+			viewProps
 			Entry types.Entry
 		}{
-			Entry: entry,
+			viewProps: props,
+			Entry:     entry,
 		}); err != nil {
 			log.Printf("Could not create edit entry page: %v", err)
 			http.Error(w, "Server issue - try again later", http.StatusInternalServerError)
